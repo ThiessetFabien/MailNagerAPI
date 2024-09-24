6d8452994687539487997eae8e7288e1a8ec4c9e
‘ThiessetFabien’ <thiessetfabienpro@gmail.com>
2024-09-24 06:48:18 +0200

    fix: mongoose connect work in progress

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -4,13 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OffersModule } from './offers.module';
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../service/app.service';
-import { DatabaseModule } from './database.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(`${process.env.DB_URL}/${process.env.DB_NAME}`),
     OffersModule,
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
