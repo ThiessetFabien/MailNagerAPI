9efaa6da8f0f6e713858a5652e3964519a5676fb
‘ThiessetFabien’ <thiessetfabienpro@gmail.com>
2024-09-30 20:49:21 +0200

    test: work in progress

--- src/service/offers/offers.service.ts
+++ src/service/offers/offers.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import { IOffer } from 'src/interface/offer.interface';
-import { Offer } from 'src/schema/offers.schema';
+import { IOffer } from '../../interface/offer.interface';
+import { Offer } from '../../schema/offers.schema';
 import { CreateOfferDto } from '../../dto/create-offer.dto';
 import { UpdateOfferDto } from '../../dto/update-offer.dto';
 @Injectable()
@@ -16,15 +16,13 @@ export class OffersService {
       return createdOffer;
   }
 
-  async updateOffer(
-    offerId: string,
-    updateOfferDto: UpdateOfferDto,
-  ): Promise<Offer> {
-    const existingOffer = await this.offerModel.findByIdAndUpdate(
-      offerId,
-      updateOfferDto,
+  async updateOffer(offerId: string, updateOfferDto: UpdateOfferDto): Promise<IOffer> {
+    const existingOffer = await this.offerModel.findByIdAndUpdate(offerId, updateOfferDto, 
       { new: true },
     );
+    if (!existingOffer) {
+      throw new Error('Offer not found');
+    }
     return existingOffer;
   }
 
@@ -35,11 +33,17 @@ export class OffersService {
 
   async findOneOffer(offerId: string): Promise<Offer> {
     const existingOffer = await this.offerModel.findById(offerId).exec();
+    if (!existingOffer) {
+      throw new Error('Offer not found');
+    }
     return existingOffer;
   }
 
   async deleteOffer(offerId: string): Promise<Offer> {
     const deletedOffer = await this.offerModel.findByIdAndDelete(offerId);
+    if (!deletedOffer) {
+      throw new Error('Offer not found');
+    }
     return deletedOffer;
   }
 }
